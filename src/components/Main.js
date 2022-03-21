79a83107a08cf9636b9499b91dfabed000137632
lemoscaio <caio_lemos94@hotmail.com>
2022-03-21 04:11:04 -0300

    loads question from json file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,67 +1,73 @@
-import React from 'react';
+import React from 'react'
 
-import Header from "./Header.js";
-import Cards from "./Cards.js";
-import Footer from "./Footer.js";
+import Header from "./Header.js"
+import Cards from "./Cards.js"
+import Footer from "./Footer.js"
+
+import questionsJSON from "./questions.json"
 
 // TODO make questions come from JSON file
-const questions = [
-    {
-        question: "O que é JSX?",
-        answer: "JSX é uma sintaxe para escrever HTML dentro do JS",
-        answered: false,
-        status: null
-    },
-    {
-        question: "O React é __",
-        answer: "Uma biblioteca JavaScript para construção de interfaces",
-        answered: false,
-        status: null
-    },
-    {
-        question: "Componentes devem iniciar com __",
-        answer: "Letra maiúscula",
-        answered: false,
-        status: null
-    },
-    {
-        question: "Podemos colocar __ dentro do JSX",
-        answer: "Expressões",
-        answered: false,
-        status: null
-    },
-    {
-        question: "O ReactDOM nos ajudar __",
-        answer: "Interagindo com a DOM para colocar componentes React na mesma",
-        answered: false,
-        status: null
-    },
-    {
-        question: "Usamos o npm para __",
-        answer: "Gerenciar os pacotes necessários e suas dependências",
-        answered: false,
-        status: null
-    },
-    {
-        question: "Usamos props para __",
-        answer: "Passar diferentes informações para componentes ",
-        answered: false,
-        status: null
-    },
-    {
-        question: "Usamos estado (state) para __",
-        answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
-        answered: false,
-        status: null
-    }
-]
+// const questions = [
+//     {
+//         question: "O que é JSX?",
+//         answer: "JSX é uma sintaxe para escrever HTML dentro do JS",
+//         answered: false,
+//         status: null
+//     },
+//     {
+//         question: "O React é __",
+//         answer: "Uma biblioteca JavaScript para construção de interfaces",
+//         answered: false,
+//         status: null
+//     },
+//     {
+//         question: "Componentes devem iniciar com __",
+//         answer: "Letra maiúscula",
+//         answered: false,
+//         status: null
+//     },
+//     {
+//         question: "Podemos colocar __ dentro do JSX",
+//         answer: "Expressões",
+//         answered: false,
+//         status: null
+//     },
+//     {
+//         question: "O ReactDOM nos ajudar __",
+//         answer: "Interagindo com a DOM para colocar componentes React na mesma",
+//         answered: false,
+//         status: null
+//     },
+//     {
+//         question: "Usamos o npm para __",
+//         answer: "Gerenciar os pacotes necessários e suas dependências",
+//         answered: false,
+//         status: null
+//     },
+//     {
+//         question: "Usamos props para __",
+//         answer: "Passar diferentes informações para componentes ",
+//         answered: false,
+//         status: null
+//     },
+//     {
+//         question: "Usamos estado (state) para __",
+//         answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+//         answered: false,
+//         status: null
+//     }
+// ]
 
 export default function Main({ images }) {
-
+    const [questions, setQuestions] = React.useState([...questionsJSON])
+    
     const [totalAnswered, setTotalAnswered] = React.useState(0)
     const [statusList, setStatusList] = React.useState([])
 
-    const shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    React.useEffect(() => {
+        const shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+        setQuestions([...shuffledQuestions])
+    }, [])
 
     return (
         <>
@@ -69,13 +75,13 @@ export default function Main({ images }) {
                 images={images}
             />
             <Cards
-                questions={shuffledQuestions}
+                questions={questions}
                 setTotalAnswered={() => setTotalAnswered(totalAnswered + 1)}
                 setStatusList={(status) => setStatusList([...statusList, status])}
                 images={images}
             />
             <Footer
-                questions={shuffledQuestions}
+                questions={questions}
                 totalAnswered={totalAnswered}
                 statusList={statusList}
                 images={images}
